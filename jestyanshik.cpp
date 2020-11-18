815a259bded04dcb787573926579d89d018b2334
Demxn999 <74660215+Demxn999@users.noreply.github.com>
2020-11-18 23:56:12 +0800

    Add files via upload

---
+++ jestyanshik.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+        int  a, b, c, d, max_a, max_b;
+        double R;
+    cout << "Введите а: ";
+        cin >> a;
+        cout << "Введите b: ";
+        cin >> b;
+        cout << "Введите c: ";
+        cin >> c;
+        cout << "Введите d: ";
+        cin >> d;
+        max_a = 0, 
+        max_b = 0;
+        cout << "Введите R: ";
+        cin >> R;
+
+    if (max_a < a) 
+    {
+        max_a = a;
+        if (max_a < b) {
+            max_a = b;
+        }
+    }
+
+    if (max_b < c) 
+    {
+        max_b = c;
+        if (max_b < d) {
+            max_b = d;
+        }
+    }
+
+    if (sqrt(max_a * max_a + max_b * max_b) <= 2 * R) {
+        cout << "да" << endl;
+    }
+    else {
+        cout << "нет" << endl;
+    }
+}
---
+++ kratnie_chisla.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int k, l, m;
+	cout << "Vvedite k, l, m: ";
+	cin >> k >> l >> m;
+	if (k > 0) {
+		if (l > 0) {
+			if (m > 0) {
+				if ((k % m == 0) || (m % k == 0))
+					cout << m << " Kratno " << k << endl;
+				if ((k % l == 0) || (l % k == 0))
+					cout << l << " Kratno " << k << endl;
+				if ((l % m == 0) || (m % l == 0))
+					cout << m << " Kratno " << l << endl;
+			}
+			else if ((k % l == 0) || (l % k == 0))
+				cout << l << " Kratno " << k << endl;
+		}
+		else if ((m > 0) && ((m % k == 0) || (k % m == 0)))
+			cout << k << " Kratno " << m << endl;
+	}
+	else if ((l > 0) && (m > 0) && ((l % m == 0) || (m % l == 0)))
+		cout << m << " Kratno " << l << endl; 
+	return 0;
+}
